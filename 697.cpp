095325d271f2b3d55bf7c3d969aa172958f64659
zongxingquan <zongxingquan@126.com>
2019-06-18 23:28:46 +0800

    add 697

--- 697.cpp
+++ 697.cpp
@@ -15,8 +15,8 @@ public:
         int cnt = 1;
         for(auto i : m)
             cnt = max(cnt, i.second);
-		if(cnt == 1) return 1;
-		if(cnt == nums.size()) return nums.size();
+        if(cnt == 1)return 1;
+        if(cnt == nums.size())return nums.size();
         vector<int> v;
         for(auto i : m)
             if(i.second == cnt)v.push_back(i.first);
