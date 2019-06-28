b509301e016b46364ff9e6be1b94fe9bdd3039b5
zongxingquan <zongxingquan@126.com>
2019-06-29 00:12:30 +0800

    add array

--- 575.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int distributeCandies(vector<int>& candies) {
-        unordered_map<int, int> m;
-        for(int i : candies)
-            ++m[i];
-        return min((candies.size() >> 1), m.size()); 
-    }
-};
