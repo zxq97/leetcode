cd5278a775a8bc5f098da2ad0b34899b62b1718c
zongxingquan <zongxingquan@126.com>
2019-07-21 00:25:17 +0800

    add 60

---
+++ sort/60.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s;
+        for(int i = 1; i <= n; ++i)
+            s += to_string(i);
+        do{
+            
+        }while(--k && next_permutation(s.begin(), s.end()));
+        return s;
+    }
+};
