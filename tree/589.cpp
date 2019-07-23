bf47bea6460b3961256b2c16f51aef9e86b5b6b7
zongxingquan <zongxingquan@126.com>
2019-07-24 00:09:09 +0800

    add 589

---
+++ tree/589.cpp
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    void dfs(Node* root, vector<int>& v)
+    {
+        if(!root)return;
+        v.push_back(root->val);
+        for(int i = 0; i < root->children.size(); ++i)
+            dfs(root->children[i], v);
+    }
+    
+    vector<int> preorder(Node* root) {
+        if(!root)return {};
+        vector<int> v;
+        dfs(root, v);
+        return v;
+    }
+};
