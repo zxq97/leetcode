6a9faad41dcfbaeaf751e3f49f16556a7ae825ed
zongxingquan <zongxingquan@126.com>
2019-07-25 23:47:24 +0800

    add 872

---
+++ tree/872.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int>& v)
+    {
+        if(!root)return;
+        if(!root->left && !root->right)v.push_back(root->val);
+        dfs(root->left, v);
+        dfs(root->right, v);
+    }
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> a, b;
+        dfs(root1, a);
+        dfs(root2, b);
+        return a == b;
+    }
+};
