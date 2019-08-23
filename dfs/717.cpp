b9709675cf93aab25de67d2dc73a433719eaa3b0
zongxingquan <zongxingquan@126.com>
2019-08-23 18:04:32 +0800

    add 912

---
+++ dfs/717.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool dfs(int i, const vector<int>& bits)
+    {
+        if(i == -1)return 1;
+        bool ans = 0;
+        if(!bits[i])
+            ans |= dfs(i - 1, bits);
+        if(i && bits[i - 1])
+            ans |= dfs(i - 2, bits);
+        return ans;
+    }
+    
+    bool isOneBitCharacter(vector<int>& bits) {
+        return dfs(bits.size() - 2, bits);
+    }
+};
